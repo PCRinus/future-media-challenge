913cd459e5100c11224fe9b6dbcee9d1bf5d30f3
Mircea Casapu <mircea.casapu@gmail.com>
2026-04-01 21:13:25 +0300

    chore: clean up initially scaffolded files

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,8 +2,6 @@ import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import mikroOrmConfig from './mikro-orm.config';
 
 @Module({
@@ -18,7 +16,5 @@ import mikroOrmConfig from './mikro-orm.config';
       discovery: { warnWhenNoEntities: false },
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
